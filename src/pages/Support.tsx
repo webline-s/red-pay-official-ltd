0407afe5b128dc7ccfb3fa88a7f71f45cc2e5cb4
webline-s <usmankatun20@gmail.com>
2026-01-15 08:10:52 +0100

    Update Support.tsx

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -10,15 +10,15 @@ import { MessageCircle, Send, Mail, MessagesSquare } from "lucide-react";
 const Support = () => {
   const [liveChatOpen, setLiveChatOpen] = useState(false);
   const handleTelegramSupport = () => {
-    window.open("https://t.me/Redpaysupport", "_blank");
+    window.open("https://t.me/Bluepaywebline", "_blank");
   };
 
   const handleWhatsAppSupport = () => {
-    window.open("https://wa.me/2349029987533", "_blank");
+    window.open("https://wa.me/2349011417160", "_blank");
   };
 
   const handleEmailSupport = () => {
-    window.location.href = "mailto:redpaycustomerservice@gmail.com";
+    window.location.href = "mailto:financial1240@gmail.com";
   };
 
   const handleLiveChat = () => {
