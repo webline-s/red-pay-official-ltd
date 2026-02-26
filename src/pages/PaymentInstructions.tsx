187c4635c918b3ee6d75241a377d225c0163ff25
webline-s <usmankatun20@gmail.com>
2026-02-26 19:13:12 +0100

    Update account number and account name in PaymentInstructions

--- src/pages/PaymentInstructions.tsx
+++ src/pages/PaymentInstructions.tsx
@@ -19,9 +19,9 @@ const PaymentInstructions = () => {
   const [showFailure, setShowFailure] = useState(false);
 
   const amount = "6,700";
-  const accountNumber = "9066084863";
+  const accountNumber = "0028434745";
   const bankName = "MOMO PSB";
-  const accountName = "ISTIFANUS DANIEL";
+  const accountName = "USMAN MOHAMMED";
   const referenceId = `REF${Date.now()}`;
 
   const copyToClipboard = (text: string, field: string) => {
