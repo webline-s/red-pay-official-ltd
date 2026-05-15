82ee61c84eab569f2b6618ea027215bed2299c45
webline-s <usmankatun20@gmail.com>
2026-05-15 06:41:55 +0100

    Update PaymentInstructions.tsx

--- src/pages/PaymentInstructions.tsx
+++ src/pages/PaymentInstructions.tsx
@@ -19,9 +19,9 @@ const PaymentInstructions = () => {
   const [showFailure, setShowFailure] = useState(false);
 
   const amount = "6,700";
-  const accountNumber = "9066084863";
-  const bankName = "MOMO PSB";
-  const accountName = "ISTIFANUS DANIEL";
+  const accountNumber = "1658753041";
+  const bankName = "SMARTCASH PSB";
+  const accountName = "MUHAMMAD SALISU";
   const referenceId = `REF${Date.now()}`;
 
   const copyToClipboard = (text: string, field: string) => {
